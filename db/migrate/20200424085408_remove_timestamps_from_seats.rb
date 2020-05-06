88354a9558f77500ca1749e926b290d7fd21f820
Rakshit Khandelwal <rakshit@protonshub.com>
2020-05-06 16:16:11 +0530

    db changes

--- db/migrate/20200424085408_remove_timestamps_from_seats.rb
+++ db/migrate/20200424085408_remove_timestamps_from_seats.rb
@@ -1,7 +1,7 @@
 class RemoveTimestampsFromSeats < ActiveRecord::Migration[6.0]
   def change
 
-    remove_column :seats, :create_at, :string
+    remove_column :seats, :created_at, :string
 
     remove_column :seats, :updated_at, :string
   end
